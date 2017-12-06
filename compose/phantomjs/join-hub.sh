bcaec5e14f671ba1168938e34108fef2506667c6
chachooon <chachooon@gmail.com>
2017-12-06 19:39:23 +0900

    .

--- compose/phantomjs/join-hub.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-HUB=${SELENIUM_HUB_ADDRESS:-hub:4444}
-until /usr/bin/phantomjs --webdriver `hostname -I | awk '{print $1}'`:8080 ${PHANTOMJS_OPTS} --webdriver-selenium-grid-hub http://${HUB}
-do
-  echo "Trying to connect to ${HUB}"
-  sleep 3
-done
