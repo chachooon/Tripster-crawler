37c03f36aca76c5100c1fdce85a45fdca02863f7
chachooon <chachooon@gmail.com>
2017-12-07 11:56:22 +0900

    클래스 기반 뷰처리

--- pjt_tripster/tripster/urls.py
+++ pjt_tripster/tripster/urls.py
@@ -3,8 +3,9 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from tripster import views
 
 urlpatterns = [
-    url(r'^$', views.tripster_list),
-    url(r'^(?P<pk>[0-9]+)/$', views.tripster_detail),
+    # 클래스를 호출하고 해당클래스의 as_view() 함수를 호출
+    url(r'^$', views.TripsterList.as_view()),
+    url(r'^(?P<pk>[0-9]+)/$', views.TripsterDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- pjt_tripster/tripster/views.py
+++ pjt_tripster/tripster/views.py
@@ -1,44 +1,14 @@
-from django.shortcuts import render
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
+# 클래스 기반의 Rest CRUD 처리
 from tripster.models import Tripster
 from tripster.serializers import TripsterSerializer
+from rest_framework import generics
 
-# 요청 url 인 tripster/ 에 대해서 urls.py 에 정의된 view.tripster_list 가 호출된다.
-@api_view(['GET', 'POST'])
-def tripster_list(request, format=None):
-    if request.method == 'GET':
-        tripster = Tripster.objects.all()
-        serializer = TripsterSerializer(tripster, many=True) # many 값이 True 이면 다수의 데이터 instance를 직렬화할수 있다
-        return Response(serializer.data)
+# generics 에 목록과 생성 API 가 정의되어 있다
+class TripsterList(generics.ListCreateAPIView):
+    queryset = Tripster.objects.all()
+    serializer_class = TripsterSerializer
 
-    elif request.method == 'POST':
-        serializer = TripsterSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-# 요청 url 인 bbs/번호 에 대해서 urls.py 에 정의된 view.bbs_detail 이 호출된다
-@api_view(['GET', 'PUT', 'DELETE'])
-def tripster_detail(request, pk, format=None):
-    try:
-        tripster = Tripster.objects.get(pk=pk)
-    except Tripster.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-    if request.method == 'GET':
-        serializer = TripsterSerializer(tripster)
-        return Response(serializer.data)
-
-    elif request.method == 'PUT':
-        serializer = TripsterSerializer(tripster, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    elif request.method == 'DELETE':
-        tripster.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+# generics 에 상세, 수정, 삭제 API가 정의되어 있다
+class BbsDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Tripster.objects.all()
+    serializer_class = TripsterSerializer
